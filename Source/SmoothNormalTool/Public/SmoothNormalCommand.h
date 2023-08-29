e11dabbbe46578e69cbbb45667efaaa36dab1fef
ydgros <65645158+ydgros@users.noreply.github.com>
2023-08-29 20:03:22 +0800

    Update SmoothNormalCommand.h

--- Source/SmoothNormalTool/Public/SmoothNormalCommand.h
+++ Source/SmoothNormalTool/Public/SmoothNormalCommand.h
@@ -12,6 +12,7 @@ class SMOOTHNORMALTOOL_API SmoothNormalCommand
 {
 private:
 	static void SmoothNormalSkeletalMesh(FAssetData AssetData);
+	static void SmoothNormalStaticMesh(FAssetData AssetData);
 	static void SmoothNormalStaticMeshTriangle(FAssetData AssetData);
 public:
 	static void SmoothNormal(TArray<FAssetData> SelectedAssets);
